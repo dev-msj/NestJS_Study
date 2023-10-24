a9c074fd68eb1e9932d4fc5cbf39151d1c2a288b
dev-msj <msj944@naver.com>
2023-10-24 22:13:34 +0900

    챕터3 주석 추가 및 불필요한 코드 제거

--- chapter3_interface/src/app.module.ts
+++ chapter3_interface/src/app.module.ts
@@ -1,22 +1,20 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { UsersController } from './users/users.controller';
-import { UsersService } from './users/users.service';
 import { UsersModule } from './users/users.module';
 import { ApiController } from './api/api.controller';
 import { ApiVersionController } from './api/api-version.controller';
 
-// sub domain을 추가할 때는 가장 최상위 controller가 가장 나중 위치에 있어야 한다.
-// 그렇지 않다면 동일한 경로를 가질 경우 무조건 최상위 controller만 인식하게 되는 것 같다.
+/**
+ * sub domain을 추가할 때는 가장 최상위 controller가 가장 나중 위치에 있어야 한다.
+ * 그렇지 않다면 동일한 경로를 가질 경우 무조건 최상위 controller만 인식하게 되는 것 같다.
+ * 예상하기로는 ApiVersionController, ApiController는 서브(하위) 도메인이라서,
+ * 같은 경로라면 controllers list를 iterate 할 때 최상위 도메인인
+ * AppController가 먼저 매칭되어 결과를 반환해버리기 때문인 것 같다.
+ */
 @Module({
   imports: [UsersModule],
-  controllers: [
-    UsersController,
-    ApiVersionController,
-    ApiController,
-    AppController,
-  ],
-  providers: [AppService, UsersService],
+  controllers: [ApiVersionController, ApiController, AppController],
+  providers: [AppService],
 })
 export class AppModule {}
