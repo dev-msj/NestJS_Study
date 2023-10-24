770eb8ec90e92a1b11709949344881659be338f4
dev-msj <msj944@naver.com>
2023-10-25 07:57:20 +0900

    챕터6 실습 완료

---
+++ chapter6_dynamic_module/src/app.module.ts
@@ -0,0 +1,34 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { UsersModule } from './users/users.module';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import emailConfig from './config/emailConfig';
+import { validationSchema } from './config/validationSchema';
+
+/**
+ * NestJS는 dotenv를 내부적으로 활용하는 @nestjs/config 패키지를 제공한다.
+ * 이를 이용해 ConfigModule을 동적으로 생성할 수 있다.
+ * ConfigModule의 forRoot()는 소비 모듈이 원하는 옵션값을 전달받아
+ * DynamicModule를 return 하는 정적 method다.
+ */
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      /**
+       * dotenv 파일이 dist로 out될 수 있도록
+       * nest-cli.json 파일에서 asset 옵션 추가했다.
+       * 그래서 런타임 때 config/env 딕셔너리에서 직접 가져올 수 있게 되었다.
+       */
+      // envFilePath: process.env.NODE_ENV === 'production' ? '.production.env' : process.env.NODE_ENV === 'stage' ? '.stage.env' : '.development.env',
+      envFilePath: [`${__dirname}/config/env/.${process.env.NODE_ENV}.env`],
+      load: [emailConfig],
+      isGlobal: true,
+      validationSchema,
+    }),
+    UsersModule,
+  ],
+  controllers: [AppController],
+  providers: [ConfigService],
+})
+export class AppModule {}
