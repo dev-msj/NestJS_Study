caca7b2a1ac7d58ea3e8034c8bfaf1da170decb6
dev-msj <msj944@naver.com>
2023-10-24 22:32:52 +0900

    챕터3 주석 추가 및 수정

--- chapter3_interface/src/app.module.ts
+++ chapter3_interface/src/app.module.ts
@@ -6,14 +6,14 @@ import { ApiController } from './api/api.controller';
 import { ApiVersionController } from './api/api-version.controller';
 
 /**
- * sub domain을 추가할 때는 가장 최상위 controller가 가장 나중 위치에 있어야 한다.
- * 그렇지 않다면 동일한 경로를 가질 경우 무조건 최상위 controller만 인식하게 되는 것 같다.
- * 예상하기로는 ApiVersionController, ApiController는 서브(하위) 도메인이라서,
- * 같은 경로라면 controllers list를 iterate 할 때 최상위 도메인인
- * AppController가 먼저 매칭되어 결과를 반환해버리기 때문인 것 같다.
+ * sub domain을 추가할 때는 루트 controllers의 controller가 가장 마지막에 있어야 한다.
+ * 서브(하위) 도메인은 root controller와 같은 엔드 포인트를 가진다.
+ * 그래서 root controller가 앞에 있을 경우, controllers list를 iterate하면서
+ * 엔드 포인트가 먼저 매칭되는 root controller를 반환해버리기 때문이다.
  */
 @Module({
   imports: [UsersModule],
+  // 신규 추가되는 controller를 list에 추가하여 등록한다.
   controllers: [ApiVersionController, ApiController, AppController],
   providers: [AppService],
 })
