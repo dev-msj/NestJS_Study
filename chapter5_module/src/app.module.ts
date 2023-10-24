bce61604dbc319765b23507efe6a825eb190f4aa
dev-msj <msj944@naver.com>
2023-10-25 06:09:53 +0900

    챕터5 불필요한 코드 제거

--- chapter5_module/src/app.module.ts
+++ chapter5_module/src/app.module.ts
@@ -1,12 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
-import { EmailModule } from './email/email.module';
 
 @Module({
-  imports: [UsersModule, EmailModule],
+  imports: [UsersModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [],
 })
 export class AppModule {}
