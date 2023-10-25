e022ead4ca856df9bf08014216a7aca01fc3ceaa
dev-msj <msj944@naver.com>
2023-10-25 09:14:33 +0900

    챕터4 속성 기반 주입 설명 오류 수정

--- chapter4_provider/src/base.service.ts
+++ chapter4_provider/src/base.service.ts
@@ -6,6 +6,9 @@ import { SampleAService } from './sample-A.service';
 export class BaseService {
   /**
    * 생성자 대신 Inject 데코레이터를 통해 속성 기반 주입을 할 수 있다.
+   * 속성 기반 주입은 주로 하위 클래스의 super() 호출을 통해
+   * 부모 클래스의 의존성을 해결해야 하는 특수한 경우에 사용되며,
+   * nestJS는 기본적으로 생성자를 통해 DI하는 것을 권장한다.
    * 커스텀 Provider를 활용하면 string, Symbol Type도 DI할 수 있다.
    */
   @Inject(SampleAService) private readonly sampleAService: SampleAService;
