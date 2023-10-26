9efb54a1f0eb3c78b646f6ace756c3542c9e2905
dev-msj <msj944@naver.com>
2023-10-26 16:30:34 +0900

    챕터5 모듈 전역 선언 데코레이터 설명 추가

--- chapter5_module/src/email/email.module.ts
+++ chapter5_module/src/email/email.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common';
 import { EmailService } from './email.service';
 
+// @Global // 모듈을 전역으로 사용하고 싶을 경우에 사용한다.
 @Module({
   // EmailModule에서 EmailService를 사용할 수 있도록 제공한다.
   providers: [EmailService],
