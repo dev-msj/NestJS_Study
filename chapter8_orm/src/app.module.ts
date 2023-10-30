0833c91cf188575d88c00ba10832f901251ef413
dev-msj <msj944@naver.com>
2023-10-30 17:25:59 +0900

    챕터8 TypeOrmConfig 파일로 분리

--- chapter8_orm/src/app.module.ts
+++ chapter8_orm/src/app.module.ts
@@ -5,6 +5,7 @@ import { ConfigModule, ConfigService } from '@nestjs/config';
 import emailConfig from './config/emailConfig';
 import { validationSchema } from './config/validationSchema';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { typeOrmConfig } from './config/typeOrmConfig.service';
 
 @Module({
   imports: [
@@ -14,36 +15,7 @@ import { TypeOrmModule } from '@nestjs/typeorm';
       isGlobal: true,
       validationSchema,
     }),
-    TypeOrmModule.forRoot({
-      type: 'mysql',
-      host: process.env.DB_HOST,
-      port: Number(process.env.DB_PORT),
-      username: process.env.DB_USERNAME,
-      password: process.env.DB_PASSWORD,
-      database: process.env.DB_DATABASE,
-      // TypeORM이 자동으로 인식할 entity class 경로
-      entities: [`${__dirname}/**/*.entity{.ts,.js}`],
-      // 서비스 구동 시 소스 코드 기반으로 DB Schema 동기화 여부
-      // true면 서비스가 시작될 때 자동으로 테이블 생성. 미리 만들어 놓으면 Error 남.
-      // synchronize: process.env.DB_SYNCHRONIZE === 'true',
-      synchronize: false,
-
-      // 연결 재시도 횟수. default: 10
-      // retryAttempts: 10,
-
-      // 재시도 간의 지연 시간(ms). default: 3000
-      // retryDelay: 3000,
-
-      // 에러가 났을 때, 인수 err를 받아서 연결을 시도할지 판단하는 함수.
-      // toRetry: (err: any) => false,
-
-      // 엔티티를 자동 로드할지 여부
-      // autoLoadEntities: false,
-
-      //연결을 재시도할 때 verbose 레벨로 에러 메시지를 출력할지 여부.
-      // verbose -> 상세 메시지
-      // verboseRetryLog: false,
-    }),
+    TypeOrmModule.forRootAsync(typeOrmConfig),
     UsersModule,
   ],
   controllers: [AppController],
