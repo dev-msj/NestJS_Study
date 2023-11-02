6464a67a056269d81a40dcac11ce42883c44a58f
dev-msj <msj944@naver.com>
2023-11-02 10:24:10 +0900

    챕터6 env 설정에 대한 주석 추가

--- chapter6_dynamic_module/src/app.module.ts
+++ chapter6_dynamic_module/src/app.module.ts
@@ -20,6 +20,17 @@ import { validationSchema } from './config/validationSchema';
        * nest-cli.json 파일에서 asset 옵션 추가했다.
        * 그래서 런타임 때 config/env 경로에서
        * env 파일을 가져올 수 있게 되었다.
+       *
+       * 책에서는 위와 같이 설명하지만
+       * 이 방법은 OS 환경에 따라서 env가 dist에
+       * 생성되지 않는 문제도 발생하기도 한다.
+       *
+       * 개인적인 생각으로는 책의 방법보다는
+       * 그냥 root path에 env 디렉토리를 만들어서
+       * envFilePath가 "env/.${process.env.NODE_ENV}.env"를
+       * 보도록 만드는 것이 더 나은 것 같다.
+       * 이렇게 하면 굳이 env file이 dist에 생성되도록
+       * 하지 않아도 되기 때문이다.
        */
       // envFilePath: process.env.NODE_ENV === 'production' ? '.production.env' : process.env.NODE_ENV === 'stage' ? '.stage.env' : '.development.env',
       envFilePath: [`${__dirname}/config/env/.${process.env.NODE_ENV}.env`],
