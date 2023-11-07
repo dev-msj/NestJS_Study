fcf2620368cbdf286aa641e4f8282f0988ace6ec
dev-msj <msj944@naver.com>
2023-11-07 11:55:17 +0900

    챕터10 가드 관련 설명 추가

--- chapter10_jwt/src/app.module.ts
+++ chapter10_jwt/src/app.module.ts
@@ -15,7 +15,18 @@ import { Logger2Middleware } from './logger/logger2.middleware';
 import { UsersController } from './users/users.controller';
 import { typeOrmConfig } from './config/typeOrmConfig.service';
 import authConfig from './config/authConfig';
+import { APP_GUARD } from '@nestjs/core';
+import { HandlerRolesGuard } from './auth/handler-roles.guard';
+import { ClassRolesGuard } from './auth/class-roles.guard';
+import { RolesGuard } from './auth/roles.guard';
 
+/**
+ * nestjs 자체 라이브러리 어딘가에 APP_GUARD라는 키워드를 받으면
+ * Guard가 전역으로 선언될 수 있도록 하는 것 같다.
+ * nestjs가 제공하는 상수가 아닌 스트링 값으로 'APP_GUARD'를 넘겨도
+ * 동일하게 적용된다.
+ * 반면 APP_GUARD가 아닌 다른 문자열을 넘기면 Guard가 동작하지 않는다.
+ */
 @Module({
   imports: [
     ConfigModule.forRoot({
@@ -35,6 +46,21 @@ import authConfig from './config/authConfig';
     //   provide: APP_GUARD,
     //   useClass: AuthGuard,
     // },
+    // {
+    //   // 라우트 핸들러에 가드 적용
+    //   provide: APP_GUARD,
+    //   useClass: HandlerRolesGuard,
+    // },
+    // {
+    //   // 클래스에 가드 적용
+    //   provide: APP_GUARD,
+    //   useClass: ClassRolesGuard,
+    // },
+    {
+      // 라우트 핸들러와 클래스 모두 가드 적용
+      provide: APP_GUARD,
+      useClass: RolesGuard,
+    },
   ],
 })
 export class AppModule implements NestModule {
--- chapter10_jwt/src/users/users.module.ts
+++ chapter10_jwt/src/users/users.module.ts
@@ -5,29 +5,10 @@ import { EmailModule } from 'src/email/email.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserEntity } from './entities/user.entity';
 import { AuthModule } from 'src/auth/auth.module';
-import { APP_GUARD } from '@nestjs/core';
-import { HandlerRolesGuard } from 'src/auth/handler-roles.guard';
-import { ClassRolesGuard } from 'src/auth/class-roles.guard';
-import { RolesGuard } from 'src/auth/roles.guard';
 
 @Module({
   imports: [EmailModule, AuthModule, TypeOrmModule.forFeature([UserEntity])],
   controllers: [UsersController],
-  providers: [
-    UsersService,
-    // {
-    //   provide: APP_GUARD,
-    //   useClass: HandlerRolesGuard,
-    // },
-    // {
-    //   provide: APP_GUARD,
-    //   useClass: ClassRolesGuard,
-    // },
-    {
-      // 라우트 핸들러와 클래스 모두 가드 적용
-      provide: APP_GUARD,
-      useClass: RolesGuard,
-    },
-  ],
+  providers: [UsersService],
 })
 export class UsersModule {}
